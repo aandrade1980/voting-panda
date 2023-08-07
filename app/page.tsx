14f2f4529f0ae4a92a6d9801fdddffea5d5830b6
Alvaro <varito1@gmail.com>
2023-08-07 10:34:15 -0300

    :tada: Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,163 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+
+import Image, { StaticImageData } from 'next/image';
+
+import { css, cx } from '@/styled-system/css';
+import { grid, gridItem, hstack, vstack } from '@/styled-system/patterns';
+
+import barbie from './assets/barbie.jpg';
+import oppenheimer from './assets/oppenheimer.jpg';
+import vs from './assets/vs.png';
+
+import { getVotes, vote } from './voting';
+import { useEffect, useState } from 'react';
+
+const ProgressBar = ({ percentage }: { percentage: number }) => (
+  <div className={css({ w: 'full', h: 6, bg: 'gray.200', rounded: 'full' })}>
+    <div
+      className={css({ h: 6, bg: 'blue.600', rounded: 'full' })}
+      style={{ width: `${percentage}%` }}
+    ></div>
+  </div>
+);
+
+const VersusImage = () => (
+  <div className={css({ position: 'relative', w: 10, h: 10 })}>
+    <Image
+      src={vs}
+      width={500}
+      height={500}
+      alt="Versus"
+      className={css({
+        position: 'absolute',
+        maxW: 'none'
+      })}
+      style={{ left: -125, top: -150, width: 300, height: 300 }}
+    />
+  </div>
+);
+
+const Card = ({
+  image,
+  onClick,
+  percentage,
+  width,
+  height,
+  alt
+}: {
+  image: StaticImageData;
+  onClick: () => void;
+  percentage: number;
+  width: number;
+  height: number;
+  alt: string;
+}) => (
+  <div
+    className={vstack({
+      bg: 'slate.500',
+      rounded: 'xl',
+      p: 5,
+      flexDir: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    })}
+    onClick={onClick}
+  >
+    <Image
+      src={image}
+      width={width}
+      height={height}
+      alt={alt}
+      className={css({
+        objectFit: 'contain',
+        objectPosition: 'center',
+        h: 96,
+        mb: 5,
+        pointerEvents: 'none'
+      })}
+    />
+    <ProgressBar percentage={percentage} />
+  </div>
+);
+
+const Header = ({ totalVotes }: { totalVotes: number }) => (
+  <header
+    className={hstack({
+      bg: 'slate.800',
+      color: 'white',
+      fontSize: '3xl',
+      shadow: 'lg',
+      px: 4,
+      py: 2,
+      roundedBottom: '2xl'
+    })}
+  >
+    <div className={css({ flexGrow: 1, fontWeight: 'bold' })}>Barbenheimer</div>
+    <div className={css({ fontWeight: 'thin' })}>{totalVotes} Votes</div>
+  </header>
+);
 
 export default function Home() {
+  const [votes, setVotes] = useState({ barbie: 0, oppenheimer: 0 });
+
+  useEffect(() => {
+    getVotes().then(setVotes);
+  }, []);
+
+  const totalVotes = votes.barbie + votes.oppenheimer;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      className={css({ mx: 'auto', maxWidth: '7xl', px: { base: 0, md: 5 } })}
+    >
+      <Header totalVotes={totalVotes} />
+
+      <div className={grid({ mt: 3, columns: { md: 11 } })}>
+        <div
+          className={gridItem({
+            colSpan: { md: 5 },
+            mx: 5,
+            md: { mx: 0, mb: 0 },
+            mb: 10
+          })}
+        >
+          <Card
+            image={barbie}
+            width={550}
+            height={800}
+            alt="Barbie Poster"
+            onClick={() => vote('barbie').then(setVotes)}
+            percentage={(votes.barbie / totalVotes) * 100}
+          />
+        </div>
+
+        <div
+          className={cx(
+            hstack({ justifyContent: 'center', alignItems: 'center' }),
+            gridItem({ colSpan: { md: 1 } })
+          )}
+        >
+          <VersusImage />
+        </div>
+
+        <div
+          className={gridItem({
+            colSpan: { md: 5 },
+            mx: 5,
+            md: { mx: 0, mb: 0 },
+            mb: 10
+          })}
+        >
+          <Card
+            image={oppenheimer}
+            width={194}
+            height={300}
+            alt="Oppenheimer Poster"
+            onClick={() => vote('oppenheimer').then(setVotes)}
+            percentage={(votes.oppenheimer / totalVotes) * 100}
+          />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
---
+++ app/voting.ts
@@ -0,0 +1,14 @@
+"use server";
+
+const votes = {
+  barbie: 3,
+  oppenheimer: 5
+}
+
+export const getVotes = () => votes;
+
+export const vote = (language: keyof typeof votes) => {
+  votes[language] += 1;
+
+  return votes;
+};
